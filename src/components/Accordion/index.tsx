8d713b589c4eed64033e42a367aeda7dc2f56bd9
Gopi <gopinath10007@gmail.com>
2023-11-17 18:02:53 +0530

    arrow icon

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,5 +1,6 @@
 import './accordion.scss';
 import React, { FC } from 'react';
+import { IoMdArrowDropright } from 'react-icons/io';
 
 export type AccordionProps = {
     id: string;
@@ -15,7 +16,10 @@ export const Accordion: FC<AccordionProps> = ({ id, name, label, content, withSh
     return (
         <div className={`accordion ${withShadow && 'shadow'} ${topBorderOnly && 'top-border-only'}`}>
             <label htmlFor={id}>
-                {label} <span>&#x3e;</span>
+                {label}{' '}
+                <span>
+                    <IoMdArrowDropright />
+                </span>
             </label>
             <input type="radio" name={name} id={id} />
             <div className={`content ${hasContentPadding && 'has-padding'}`}>{Array.isArray(content) ? content.map((text, i) => <p key={i}>{text}</p>) : content}</div>
